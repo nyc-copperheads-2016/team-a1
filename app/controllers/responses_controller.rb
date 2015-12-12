d03ee99197e3f05f853bc90394ab42fcab1e827d
Arcangelo Petretta <arcpetretta@gmail.com>
2015-12-11 20:29:29 -0500

    complete survey functionality. Still needs validations to prevent users from taking survey twice.

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,7 +1,7 @@
 post '/responses' do
   response = Response.new(params[:response])
   if response.save
-    redirect "/surveys/#{response.survey_id}/questions/1"
+    redirect "/surveys/#{response.survey_id}/questions/#{response.survey.questions.first.id}"
   else
     redirect '/?errors=unable_to_initialize_survey'
   end
--- app/controllers/selections_controller.rb
+++ app/controllers/selections_controller.rb
@@ -3,7 +3,9 @@ post '/selections' do
   if selection.save && selection.response.selections.count < selection.response.survey.questions.count
     question_id = Selection.next_question(selection)
     redirect "/surveys/#{selection.response.survey_id}/questions/#{question_id}"
-  else
+  elsif selection.response.selections.count == selection.response.survey.questions.count
     redirect "/surveys/#{selection.response.survey.id}/end"
+  else
+    redirect"/?errors=something_went_wrong"
   end
 end
