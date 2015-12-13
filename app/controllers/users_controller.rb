d5c854de96275f1a1e43d6eadfdc72336e598a3b
Jeff George <webdevjeffus@gmail.com>
2015-12-13 15:21:15 -0500

    Implement separate new user and login pages.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,24 @@
+get '/users/new' do
+  erb :"/user/new"
+end
+
+
 get '/users/:id' do
   @user = User.find_by(id: params[:id])
   @surveys = @user.surveys
   erb :'user/show'
 end
 
+
 post '/users' do
   user = User.new(params)
+
   if user.save
+    session[:user_id] = user.id
     redirect '/'
   else
-    erb :'/sessions/new', locals: { errors: user.errors.full_messages }
+    redirect '/sessions/new?errors=incorrect_user_or_password'
   end
+
+
 end
