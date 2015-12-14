45ad568ad296b7259ab6082e6c4e5f2a1a3b88b5
Eunice Do <eunicedx@gmail.com>
2015-12-13 19:13:33 -0500

    Add 401 stat page to deny users access to other user's profiles.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,9 @@ end
 
 
 get '/users/:id' do
+  if session[:user_id] != params[:id].to_i
+    halt 401, "Not authorized to view user #{params[:id]}.\n"
+  end
   @user = User.find_by(id: params[:id])
   @surveys = @user.surveys
   erb :'user/show'
